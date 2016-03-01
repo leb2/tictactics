46c3abef3ebbdb02a44ca32efcc5032bf483c5e6
Brendan Le <le.brendan@gmail.com>
2016-03-01 03:47:14 -0500

    Update static collected location

--- tictactics/settings.py
+++ tictactics/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticcollected'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticcollected')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
